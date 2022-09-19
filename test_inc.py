113111974f98d47a77ea42bb0ebfe2fd28e5a9bc
maykj529 <492446221@qq.com>
2022-09-19 21:05:37 +0800

    add error scenario

--- test_inc.py
+++ test_inc.py
@@ -15,15 +15,15 @@ class TestFun:
 
     @pytest.mark.may
     @pytest.mark.smoke
-    @pytest.mark.parametrize("a,b", [(-1, -2)])
+    @pytest.mark.parametrize("a,b", [(-11, -22)])
     def test_negative_integer(self, a, b):
-        assert inc(a, b) == -3
+        assert inc(a, b) == -33
 
     @pytest.mark.may
     @pytest.mark.smoke
-    @pytest.mark.parametrize("a,b", [(1, -2)])
+    @pytest.mark.parametrize("a,b", [(18, -20)])
     def test_positive_plus_negative(self, a, b):
-        assert inc(a, b) == -1
+        assert inc(a, b) == -2
 
     @pytest.mark.may
     @pytest.mark.smoke
@@ -32,7 +32,10 @@ class TestFun:
         assert inc(a, b) == 3.5
 
     @pytest.mark.may
-    @pytest.mark.illegal
+    @pytest.mark.error
     @pytest.mark.parametrize("a,b", [(3, 3)])
     def test_error(self, a, b):
-        assert inc(a, b) == 3
+        try:
+            assert inc(a, b) == 3
+        except BaseException as e:
+            print('\n', "wrong result")
