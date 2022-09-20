d3f4499c49440c914679e40e44b117edb042dbee
may <43255236+maykj529@users.noreply.github.com>
2022-09-20 17:29:10 +0800

    update function name

--- test_inc.py
+++ test_inc.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-def inc(a, b):
+def add(a, b):
     return a + b
 
 
@@ -11,31 +11,31 @@ class TestFun:
     @pytest.mark.smoke
     @pytest.mark.parametrize("a,b", [(1, 2)])
     def test_positive_integer(self, a, b):
-        assert inc(a, b) == 3
+        assert add(a, b) == 3
 
     @pytest.mark.may
     @pytest.mark.smoke
     @pytest.mark.parametrize("a,b", [(-11, -22)])
     def test_negative_integer(self, a, b):
-        assert inc(a, b) == -33
+        assert add(a, b) == -33
 
     @pytest.mark.may
     @pytest.mark.logic
     @pytest.mark.parametrize("a,b", [(18, -20)])
     def test_positive_plus_negative(self, a, b):
-        assert inc(a, b) == -2
+        assert add(a, b) == -2
 
     @pytest.mark.may
     @pytest.mark.logic
     @pytest.mark.parametrize("a,b", [(1.2, 2.3)])
     def test_decimal(self, a, b):
-        assert inc(a, b) == 3.5
+        assert add(a, b) == 3.5
 
     @pytest.mark.may
     @pytest.mark.error
     @pytest.mark.parametrize("a,b", [(3, 3)])
     def test_error(self, a, b):
         try:
-            assert inc(a, b) == 3
+            assert add(a, b) == 3
         except BaseException as e:
             print('\n', "wrong result")
